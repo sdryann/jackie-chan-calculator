1b2883c6bc47bd0bc11fdd14d83514bb535cc3d3
Rian Sousa <orionplay9@gmail.com>
2021-03-19 13:31:06 -0300

    initial program

---
+++ src/entities/MatInteger.java
@@ -0,0 +1,17 @@
+package entities;
+
+public class MatInteger {
+	
+	private Integer l , c;
+	
+	public MatInteger() {
+	}
+	
+	public MatInteger(int x,int y){
+		Integer[][] matrix = new Integer[x][y];
+		
+		
+		
+	}
+
+}
